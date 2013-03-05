d4a488a6ef16bcb1a52d9b336f13422b4299fd02
William Byrd <webyrd@cs.indiana.edu>
2013-03-04 18:18:28 -0700

    updated variable names

--- miniKanren-version/demo-tests.scm
+++ miniKanren-version/demo-tests.scm
@@ -2,7 +2,7 @@
 
 (test-check "demo-1"
   (run* (q)
-    (fresh (ctx c l)
+    (fresh (ctx cmd l)
       (== `((i MAXINT secret) (,PUBLIC ,PUBLIC ,SECRET)) ctx)
       (== `(seq
              (while (< i MAXINT)
@@ -11,27 +11,27 @@
              (seq
                (output ,PUBLIC (intexp ,(build-num 0)))
                (assign i (+ i (intexp ,(build-num 1))))))
-          c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+          cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '())
 
 (test-check "demo-2"
   (run* (q)
-    (fresh (ctx c l)
+    (fresh (ctx cmd l)
       (== `((i secret) (,SECRET ,SECRET)) ctx)
       (== `(seq
              (while (< i secret)
                (assign i (+ i (intexp ,(build-num 1)))))
              (output ,PUBLIC (intexp ,(build-num 0))))
-          c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+          cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '())
 
 (test-check "demo-3"
   (run* (q)
-    (fresh (ctx c l)
+    (fresh (ctx cmd l)
       (== `((i secret) (,SECRET ,SECRET)) ctx)
       (== `(seq
              (while (< i secret)
@@ -39,52 +39,52 @@
                  (assign i (+ i (intexp ,(build-num 1))))
                  (output ,PUBLIC (intexp ,(build-num 0)))))
              (output ,PUBLIC (intexp ,(build-num 0))))
-          c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+          cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '())
 
 (test-check "demo-4"
   (run* (q)
-    (fresh (ctx c l)
+    (fresh (ctx cmd l)
       (== `((i secret) (,SECRET ,SECRET)) ctx)
       (== '(while (< i secret)
              (assign i (+ i (intexp (1)))))
-          c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+          cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '((((i secret) (SECRET SECRET))
      (while (< i secret) (assign i (+ i (intexp (1)))))
      SECRET)))
 
 (test-check "demo-5"
   (run* (q)
-    (fresh (ctx c l)
+    (fresh (ctx cmd l)
       (== `((i secret) (,PUBLIC ,SECRET)) ctx)
       (== `(seq
              (while (< i secret)
                (assign i (+ i (intexp ,(build-num 1)))))
              (output ,PUBLIC (intexp ,(build-num 0))))
-          c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+          cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '())
 
 (test-check "loop-1"
   (run* (q)
-    (fresh (ctx c l il secretl)
+    (fresh (ctx cmd l il secretl)
       (== `((secret i) (,il ,secretl)) ctx)
       (== `(while (< i secret)
              (assign i (+ i (intexp ,(build-num 1)))))
-          c)
-      (typeo ctx c l)
+          cmd)
+      (typeo ctx cmd l)
       (== `(,ctx ,l) q)))
   '((((secret i) (_.0 SECRET)) SECRET)
     (((secret i) (PUBLIC PUBLIC)) PUBLIC)))
 
 (test-check "loop-2"
   (run* (q)
-    (fresh (ctx c l xl yl)
+    (fresh (ctx cmd l xl yl)
       (== `((x y) (,xl ,yl)) ctx)
       (== `(while (< x (intexp ,(build-num 10)))
              (seq
@@ -93,8 +93,8 @@
                  (while (< y (intexp ,(build-num 10)))
                    (assign y (+ y (intexp ,(build-num 1)))))
                  (assign x (+ x (intexp ,(build-num 1)))))))
-          c)
-      (typeo ctx c l)
+          cmd)
+      (typeo ctx cmd l)
       (== `(,ctx ,l) q)))
   '((((x y) (SECRET SECRET))
      SECRET)
--- miniKanren-version/examples-tests.scm
+++ miniKanren-version/examples-tests.scm
@@ -3,21 +3,21 @@
 (test-check "almost-example-1"
 ;;; uses !-o instead of typeo
   (run* (q)
-    (fresh (ctx pc c l)
+    (fresh (ctx pc cmd l)
       (== `((h low) (,SECRET ,PUBLIC)) ctx)
-      (== `(seq (while (< (intexp ()) h) (assign h (- h low))) (output ,PUBLIC (intexp (1)))) c)
-      (!-o ctx pc c l)
-      (== `(,ctx ,pc ,c ,l) q)))
+      (== `(seq (while (< (intexp ()) h) (assign h (- h low))) (output ,PUBLIC (intexp (1)))) cmd)
+      (!-o ctx pc cmd l)
+      (== `(,ctx ,pc ,cmd ,l) q)))
   '())
 
 (test-check "almost-example-2"
 ;;; uses !-o instead of typeo  
   (run* (q)
-    (fresh (ctx pc c l)
+    (fresh (ctx pc cmd l)
       (== `((h low) (,SECRET ,PUBLIC)) ctx)
-      (== `(seq (cast p-tag (while (< (intexp ()) h) (assign h (- h low)))) (output ,PUBLIC (intexp (1)))) c)
-      (!-o ctx pc c l)
-      (== `(,ctx ,pc ,c ,l) q)))
+      (== `(seq (cast p-tag (while (< (intexp ()) h) (assign h (- h low)))) (output ,PUBLIC (intexp (1)))) cmd)
+      (!-o ctx pc cmd l)
+      (== `(,ctx ,pc ,cmd ,l) q)))
   '((((h low) (SECRET PUBLIC))
      PUBLIC
      (seq (cast
@@ -28,20 +28,20 @@
 
 (test-check "example-1"
   (run* (q)
-    (fresh (ctx pc c l)
+    (fresh (ctx pc cmd l)
       (== `((h low) (,SECRET ,PUBLIC)) ctx)
-      (== `(seq (while (< (intexp ()) h) (assign h (- h low))) (output ,PUBLIC (intexp (1)))) c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+      (== `(seq (while (< (intexp ()) h) (assign h (- h low))) (output ,PUBLIC (intexp (1)))) cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '())
 
 (test-check "example-2"
   (run* (q)
-    (fresh (ctx pc c l)
+    (fresh (ctx pc cmd l)
       (== `((h low) (,SECRET ,PUBLIC)) ctx)
-      (== `(seq (cast p-tag (while (< (intexp ()) h) (assign h (- h low)))) (output ,PUBLIC (intexp (1)))) c)
-      (typeo ctx c l)
-      (== `(,ctx ,c ,l) q)))
+      (== `(seq (cast p-tag (while (< (intexp ()) h) (assign h (- h low)))) (output ,PUBLIC (intexp (1)))) cmd)
+      (typeo ctx cmd l)
+      (== `(,ctx ,cmd ,l) q)))
   '((((h low) (SECRET PUBLIC))
      (seq (cast
            p-tag
