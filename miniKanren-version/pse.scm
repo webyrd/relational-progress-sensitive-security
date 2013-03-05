50a19c14b6a98d7449449788a1a845629d1bbe8d
William Byrd <webyrd@cs.indiana.edu>
2013-03-04 17:32:56 -0700

    finished adding tests from Demo.hs

--- miniKanren-version/pse.scm
+++ miniKanren-version/pse.scm
@@ -511,12 +511,83 @@
       (== `(,gamma ,c ,l) q)))
   '((((i) (HIGH)) (assign i (+ i (intexp (1)))) LOW)))
 
+;;; Tests from Demo.hs
+(test-check "demo-1"
+  (run* (q)
+    (fresh (gamma c l)
+      (== `((i MAXINT secret) (,LOW ,LOW ,HIGH)) gamma)
+      (== `(seq
+             (while (> MAXINT i)
+               (while (> secret i)
+                 skip))
+             (seq
+               (output ,LOW (intexp ,(build-num 0)))
+               (assign i (+ i (intexp ,(build-num 1))))))
+          c)
+      (typeo gamma c l)
+      (== `(,gamma ,c ,l) q)))
+  '())
+
+(test-check "demo-2"
+  (run* (q)
+    (fresh (gamma c l)
+      (== `((i secret) (,HIGH ,HIGH)) gamma)
+      (== `(seq
+             (while (> secret i)
+               (assign i (+ i (intexp ,(build-num 1)))))
+             (output ,LOW (intexp ,(build-num 0))))
+          c)
+      (typeo gamma c l)
+      (== `(,gamma ,c ,l) q)))
+  '())
+
+(test-check "demo-3"
+  (run* (q)
+    (fresh (gamma c l)
+      (== `((i secret) (,HIGH ,HIGH)) gamma)
+      (== `(seq
+             (while (> secret i)
+               (seq
+                 (assign i (+ i (intexp ,(build-num 1))))
+                 (output ,LOW (intexp ,(build-num 0)))))
+             (output ,LOW (intexp ,(build-num 0))))
+          c)
+      (typeo gamma c l)
+      (== `(,gamma ,c ,l) q)))
+  '())
+
+(test-check "demo-4"
+  (run* (q)
+    (fresh (gamma c l)
+      (== `((i secret) (,HIGH ,HIGH)) gamma)
+      (== '(while (> secret i)
+             (assign i (+ i (intexp (1)))))
+          c)
+      (typeo gamma c l)
+      (== `(,gamma ,c ,l) q)))
+  '((((i secret) (HIGH HIGH))
+     (while (> secret i) (assign i (+ i (intexp (1)))))
+     HIGH)))
+
+(test-check "demo-5"
+  (run* (q)
+    (fresh (gamma c l)
+      (== `((i secret) (,LOW ,HIGH)) gamma)
+      (== `(seq
+             (while (> secret i)
+               (assign i (+ i (intexp ,(build-num 1)))))
+             (output ,LOW (intexp ,(build-num 0))))
+          c)
+      (typeo gamma c l)
+      (== `(,gamma ,c ,l) q)))
+  '())
+
 (test-check "loop-1"
   (run* (q)
     (fresh (gamma c l il secretl)
       (== `((secret i) (,il ,secretl)) gamma)
-      (== '(while (> secret i)
-             (assign i (+ i (intexp (1)))))
+      (== `(while (> secret i)
+             (assign i (+ i (intexp ,(build-num 1)))))
           c)
       (typeo gamma c l)
       (== `(,gamma ,c ,l) q)))
@@ -562,16 +633,3 @@
                    (assign y (+ y (intexp (1)))))
                  (assign x (+ x (intexp (1)))))))
      LOW)))
-
-(test-check "demo-4"
-  (run* (q)
-    (fresh (gamma c l)
-      (== `((i secret) (,HIGH ,HIGH)) gamma)
-      (== '(while (> secret i)
-             (assign i (+ i (intexp (1)))))
-          c)
-      (typeo gamma c l)
-      (== `(,gamma ,c ,l) q)))
-  '((((i secret) (HIGH HIGH))
-     (while (> secret i) (assign i (+ i (intexp (1)))))
-     HIGH)))
